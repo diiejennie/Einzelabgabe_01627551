0d7930e8e1d662640947b15d57ed668bedc37c07
diiejennie <jekomar@edu.aau.at>
2019-03-12 12:16:15 +0100

    Projekt erstellt

---
+++ SE2_Einzelabgabe_01627551/app/src/main/java/com/diiejennie/se2_einzelabgabe_01627551/MainActivity.java
@@ -0,0 +1,13 @@
+package com.diiejennie.se2_einzelabgabe_01627551;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
